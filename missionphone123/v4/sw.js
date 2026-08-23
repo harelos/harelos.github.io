47d5ed4d8e7b109503e55bb64c99fbd1e941db46
Jacob <harel1990@gmail.com>
2026-08-23 19:27:20 -0300

    Refresh OX V4 PWA cache for final build

--- missionphone123/v4/sw.js
+++ missionphone123/v4/sw.js
@@ -1,5 +1,5 @@
-const CACHE='ox-v4-20260823-2';
-const SHELL=['./','./index.html','./style-1.css','./style-2.css','./style-3.css','./app-loader.js','./app-1.txt','./app-2.txt','./app-3.txt','./app-4.txt','./app-5.txt','./app-6.txt','./manifest.webmanifest','../icon.svg'];
+const CACHE='ox-v4-20260823-3';
+const SHELL=['./','./index.html','./style-1.css','./style-2.css','./style-3.css','./style-4.css','./app-loader.js','./app-1.txt','./app-2.txt','./app-3.txt','./app-4.txt','./app-5.txt','./app-6.txt','./manifest.webmanifest','../icon.svg'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',event=>{
